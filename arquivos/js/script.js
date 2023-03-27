c8806cf93f5b08d4f16e3a87ba1199818112ed67
Renan <renan@pop-os.localdomain>
2023-03-27 16:32:29 -0300

    mudanças realizadas

--- arquivos/js/script.js
+++ arquivos/js/script.js
@@ -1,10 +1,16 @@
 function changeTheme() {
     const btn_theme = document.getElementById('myRange').value;
-
-    console.log(btn_theme)
+    let display = document.querySelector('.container_display');  
+    let container_numbers = document.querySelector('.container_numbers');
+    let reset = document.querySelector('.reset');
+    let del = document.querySelector('.del');    
 
     if (btn_theme === '2') {
-        document.body.classList.add('body_them-two');
+        document.body.classList.add('body-2');        
+        display.classList.add('container-display-2')
+        container_numbers.classList.add('container_numbers-2');
+        reset.classList.add('reset-2')
+        del.classList.add('del-2')
     }
 
 }
